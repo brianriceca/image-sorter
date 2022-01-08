80e19682c203bd9a41e90f5f4aaa3a8be815e7b0
Brian Rice <brice@bigloops.com>
2022-01-07 19:05:57 -0800

    looks like tkinter and Qt do keyboard events differently, sometimes single chars, sometimes chars:scancode

--- image-sorter.py
+++ image-sorter.py
@@ -2,13 +2,16 @@
 
 import PySimpleGUI as sg                        
 import os.path
-import PIL.Image
+import PIL.Image as pilimage
 import sys
 import base64
 import json
 import io
 
-def convert_to_bytes(file_or_bytes, resize=None):
+default_size = (600,600)
+image_types = (".png", ".jpg", ".jpeg", ".tiff", ".bmp")
+
+def convert_to_bytes(file_or_bytes, resize=None, dirpath=None):
     '''
     Will convert into bytes and optionally resize an image that is a file or a base64 bytes object.
     Turns into  PNG format in the process so that can be displayed by tkinter
@@ -16,28 +19,31 @@ def convert_to_bytes(file_or_bytes, resize=None):
     :type file_or_bytes:  (Union[str, bytes])
     :param resize:  optional new size
     :type resize: (Tuple[int, int] or None)
+    :param dirpath: directory path to file
     :return: (bytes) a byte-string object
     :rtype: (bytes)
     '''
     if isinstance(file_or_bytes, str):
-        img = PIL.Image.open(file_or_bytes)
+        if dirpath is None:
+            img = pilimage.open(file_or_bytes)
+        else:
+            img = pilimage.open(os.path.join(dirpath,file_or_bytes))
     else:
         try:
-            img = PIL.Image.open(io.BytesIO(base64.b64decode(file_or_bytes)))
+            img = pilimage.open(io.BytesIO(base64.b64decode(file_or_bytes)))
         except Exception as e:
             dataBytesIO = io.BytesIO(file_or_bytes)
-            img = PIL.Image.open(dataBytesIO)
+            img = pilimage.open(dataBytesIO)
 
     cur_width, cur_height = img.size
     if resize:
         new_width, new_height = resize
         scale = min(new_height/cur_height, new_width/cur_width)
-        img = img.resize((int(cur_width*scale), int(cur_height*scale)), PIL.Image.ANTIALIAS)
+        img = img.resize((int(cur_width*scale), int(cur_height*scale)), pilimage.ANTIALIAS)
     with io.BytesIO() as bio:
         img.save(bio, format="PNG")
         del img
         return bio.getvalue()
-
 
 if len(sys.argv) > 1:
   folder = sys.argv[1]
@@ -50,24 +56,45 @@ except:
   file_list = []
 
 fnames = [f for f in file_list if os.path.isfile(
-            os.path.join(folder, f)) and f.lower().endswith((".png", ".jpg", ".jpeg", ".tiff", ".bmp"))]
+            os.path.join(folder, f)) and f.lower().endswith(image_types)
+            and not f.startswith('.')
+          ]
 
 if len(fnames) == 0:
   raise RuntimeError(f'No image files in {folder}')
 
-with open('keys-and-dirs.json',encoding='utf-8') as f:
-  directory_for_key = json.load(f)
+def dict_raise_on_duplicates(ordered_pairs):
+  """Reject duplicate keys."""
+  d = {}
+  for k, v in ordered_pairs:
+    if k in d:
+      raise ValueError("duplicate key: %r" % (k,))
+    else:
+      d[k] = v
+  return d
 
-set_of_keys = directory_for_key.keys()
+with open('keys-and-dirs.json',encoding='utf-8') as f:
+  directory_for_key = json.load(f,object_pairs_hook=dict_raise_on_duplicates)
+
+if len(directory_for_key) == 0:
+  raise RuntimeError(f'No action keys defined')
+
+if 'q' in directory_for_key:
+  raise RuntimeError(f'Sorry, q means quit')
+
+if len(directory_for_key.keys()) != len(set(directory_for_key.keys())):
+  raise RuntimeError(f'Duplicate key(s) defined')
+  
+set_of_keys = set(directory_for_key.keys())
 
 sg.theme('DarkGreen')
 
 imagepointer = 0
 
 layout = [  [sg.Text(f"Simple image sorter: {fnames[imagepointer]}", key='-TITLE-')],
-            [sg.Text('', size=(18,1), key='-FEEDBACK-')],
-            [sg.Image(convert_to_bytes(fnames[imagepointer]),key='-IMAGE-')],
-            [sg.Button('Prev'), sg.Button('Next'), sg.Button('Quit')] ]
+            [sg.Text('', size=(50,1), key='-FEEDBACK-')],
+            [sg.Image(convert_to_bytes(fnames[imagepointer],resize=default_size,dirpath=folder),key='-IMAGE-')],
+            [sg.Button('Prev'), sg.Button('Next'), sg.Button('Delete',button_color=('#FFFFFF','#FF0000')),sg.Button('Quit')] ]
 
 window = sg.Window('Image Sorter', layout, resizable=True,
              return_keyboard_events=True, use_default_focus=False)
@@ -76,29 +103,21 @@ of = open('eventlog.txt','a')
                                                 
 while True:
   event, values = window.read()
-  if event == sg.WIN_CLOSED or event == 'Quit' or event is None:
+  if (event == sg.WIN_CLOSED or event is None or
+      event == 'Quit' or event.startswith('q')):
     break
-  if len(event) == 1:
-    window['-FEEDBACK-'].update(value=f"Got char {event}")
-    continue
-  if str(event).startswith('Right:') or event == 'Prev':
-    imagepointer = imagepointer-1 if imagepointer > 0 else len(fnames)
+  window['-FEEDBACK-'].update(value=f"Got event '{event}'")
+  if str(event).startswith('Left:') or event == 'Prev':
+    imagepointer = imagepointer-1 if imagepointer > 0 else len(fnames)-1
     window['-FEEDBACK-'].update(f'imagepointer is {imagepointer}')
     window['-TITLE-'].update(f"Simple image sorter: {fnames[imagepointer]}")
-    window['-IMAGE-'].update(data=convert_to_bytes(fnames[imagepointer]))
+    window['-IMAGE-'].update(data=convert_to_bytes(fnames[imagepointer],resize=default_size,dirpath=folder))
     continue
-  if str(event).startswith('Left:') or event == 'Next':
-    imagepointer = imagepointer+1 if imagepointer < len(fnames) else 0
+  if str(event).startswith('Right:') or event == 'Next':
+    imagepointer = imagepointer+1 if imagepointer < len(fnames)-1 else 0
     window['-FEEDBACK-'].update(f'imagepointer is {imagepointer}')
     window['-TITLE-'].update(f"Simple image sorter: {fnames[imagepointer]}")
-    window['-IMAGE-'].update(data=convert_to_bytes(fnames[imagepointer]))
+    window['-IMAGE-'].update(data=convert_to_bytes(fnames[imagepointer],resize=default_size,dirpath=folder))
     continue
-  window['-FEEDBACK-'].update(value=f"I got '{event}'")
-  print(event , file=of)
-#    if event in set_of_keys:
-#      window['-FEEDBACK-'].update(value=f"You want me to move this to {directory_for_key[event]}")
-#    else:
-#      window['-FEEDBACK-'].update(value=f"I don't recognize {event}")
-
-window.close()
-of.close()
+  if event in set_of_keys:
+    window['-FEEDBACK-'].update(value=f"You want to move this to '{directory_for_key[event]}'")
